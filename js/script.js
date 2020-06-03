901ea806cb20d9d5a8b2b1852846058106c4422f
steve-lch <61156218+steve-lch@users.noreply.github.com>
2020-06-03 23:34:40 +0800

    update0603

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$('#graphics').flipLightBox();
+$('.galleries').flipLightBox();
 
 $('.menu-bars').click(function(e){
 		e.preventDefault();
@@ -8,7 +8,7 @@ $('.menu-bars').click(function(e){
 			m.stop().animate({width:'0%',height:'0%'},500);
 		}
 		else {
-			m.stop().animate({width:'100%',height:'100%'},500);
+			m.stop().animate({width:'50%',height:'70vh'},500);
 		}
 		m.toggleClass('.extended');
 		return false;
